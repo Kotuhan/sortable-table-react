d425b16919f908fe6bc6fd16ccdf8c1d5594c563
Kotuhan <kotuchi0203@gmail.com>
2017-02-01 15:03:20 +0200

    add rc-table

--- src/SortableTable.jsx
+++ src/SortableTable.jsx
@@ -1,6 +1,23 @@
 import React, { Component } from 'react';
+import Table from 'rc-table';
 import shallowCompare from 'react-addons-shallow-compare';
-import styles from './index.styl';
+//import styles from './index.styl';
+
+const columns = [
+    {
+        title: 'Name', dataIndex: 'name', key: 'name', width: 100
+    },
+    {
+        title: 'Age', dataIndex: 'age', key: 'age', width: 100
+    }, {
+        title: 'Address', dataIndex: 'address', key: 'address', width: 200
+    }];
+
+const data = [
+    { name: 'Jack', age: 28, address: 'some where', key: '1' },
+    { name: 'Rose', age: 36, address: 'some where', key: '2' }
+];
+
 
 export default class extends Component {
     static propTypes = {
@@ -10,15 +27,17 @@ export default class extends Component {
 
     state = {
     };
-    actions = {
-    };
+
 
     shouldComponentUpdate(nextProps, nextState) {
         return shallowCompare(this, nextProps, nextState);
     }
+
     render() {
         return (
-            <div className={styles.componentTitle}>Hello</div>
+            <div className="container">
+                <Table columns={columns} data={data} />
+            </div>
         );
     }
 }
